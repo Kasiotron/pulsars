2313b1f2ee40eaa083a0a53b6a5403a38f1602dd
Kasia <kasia.kasprzak@vp.pl>
2020-04-02 11:50:24 +0200

    New changes

--- database/urls.py
+++ database/urls.py
@@ -4,11 +4,5 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('fill', views.fill, name='fill'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('fill', views.fill, name='fill')
 ]

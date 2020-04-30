9ae7548c426d41d3b0102b270766bae93744440a
Andrzej Szary <aszary@gmail.com>
2020-04-30 10:51:42 +0200

    pulsars added

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Pulsar(models.Model):
-    Name = models.CharField(max_length=200)
+    Name = models.CharField(max_length=200, help_text="Pulsar name.  The B name if exists, otherwise the J name.")
--- database/views.py
+++ database/views.py
@@ -10,17 +10,22 @@ def fill(request):
         psrs = line.strip().split(',')
         for psr in psrs:
             if not "READ" in psr and not "search" in psr and not "J0000" in psr and psr != '':
-                pulsars.append(psr)
+                pulsars.append(psr.strip())
         if "README.txt" in line:
             break
     f.close()
 
-    # for ps in pulsars:
-    #     psrs = Pulsar.object.filter(Name=ps)
-    #     if len(psr) == 0:
-    #         pass
-    #     else:
-    #         pass
-    # print(len(psrs))
 
-    return HttpResponse("Database changes <br /> {}".format(pulsars))
+    for ps in pulsars:
+        p = Pulsar.objects.filter(Name=ps)
+        if len(p) == 0:
+            psr = Pulsar(Name=ps)
+            psr.save()
+        else:
+            # new values
+
+            #print("already added")
+            pass
+
+
+    return HttpResponse("Database changes <br /> {}".format(1))

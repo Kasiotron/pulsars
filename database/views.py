b707f68f9e8c194901788115b2bcba705ca875a4
Kasia <kasia.kasprzak@vp.pl>
2020-04-30 10:17:57 +0200

    Blank pulsars removed

--- database/views.py
+++ database/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from database.models import Pulsar
 
 def fill(request):
     f = open("database/static/list.txt")
     lines = f.readlines()
     pulsars = []
     for line in lines:
-        psrs = line.rstrip().split(',')
+        psrs = line.strip().split(',')
         for psr in psrs:
-            if not "READ" in psr and not "search" in psr and not "J0000" in psr:
+            if not "READ" in psr and not "search" in psr and not "J0000" in psr and psr != '':
                 pulsars.append(psr)
         if "README.txt" in line:
             break
     f.close()
+
+    # for ps in pulsars:
+    #     psrs = Pulsar.object.filter(Name=ps)
+    #     if len(psr) == 0:
+    #         pass
+    #     else:
+    #         pass
+    # print(len(psrs))
+
     return HttpResponse("Database changes <br /> {}".format(pulsars))
